ac283fa917d6c427d9923e4451b96bc412ce6f7b
Jens Kraemer <jk@jkraemer.net>
2017-09-12 11:34:48 +0800

    require login for everything #423
    - except terms and conditions, sign up / sign in etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, if: :login_required?
+
+  private
+
+  def login_required?
+    unless params[:controller] == 'decidim/pages' and
+      params[:action] == 'show' and
+      params[:id] == 'terms-and-conditions'
+
+      return true
+    end
+  end
 end
