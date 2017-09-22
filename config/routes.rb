a81e2af2cec8a6e635a44122710e21f30446e0b9
Jens Kraemer <jk@jkraemer.net>
2017-09-22 15:04:13 +0800

    re-routes unused devise endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  # re-route some devise endpoints that are unused
+  get '/users/edit',    to: redirect('/')
+  get '/users/sign_up', to: redirect('/')
+  match '/users', to: redirect('/'), via: %i(put patch post delete)
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
