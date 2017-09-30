5dc9689cc92c209a67f5aed3265dae12ace2aca0
Jens Kraemer <jk@jkraemer.net>
2017-09-30 10:35:41 +0800

    ease selector for chosen in account form
    - the form does not appear to have the edit_user class when there was an error.

--- app/assets/javascripts/fundaction.js
+++ app/assets/javascripts/fundaction.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  $('form.edit_user select#user_languages').chosen({ width: '100%' });
-  $('form.edit_user select#user_country').chosen({ width: '100%' });
-  $('form.edit_user select#user_areas_of_interest').chosen({ width: '100%' });
+  $('form select#user_languages').chosen({ width: '100%' });
+  $('form select#user_country').chosen({ width: '100%' });
+  $('form select#user_areas_of_interest').chosen({ width: '100%' });
 });
