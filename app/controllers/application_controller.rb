2f005bce210f49e3761ae4c634a5902e02e39861
Jens Kraemer <jk@jkraemer.net>
2017-12-08 10:06:23 +0800

    always require login even if decidim engine controllers try to do otherwise #450

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, if: :login_required?
+  before_action :fund_action_authenticate_user!, if: :login_required?
 
   private
+
+  # the aliasing prevents i.e. proposals_controller to change the condition
+  alias fund_action_authenticate_user! authenticate_user!
 
   def login_required?
     unless %w(
