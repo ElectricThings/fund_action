b02843604e7ba53df67d85ec54ba3a00165b7c8c
Jens Kraemer <jk@jkraemer.net>
2018-02-09 17:40:33 +0800

    airbrake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :fund_action_authenticate_user!, if: :login_required?
 
+  rescue_from StandardError, with: :report_error
+
   private
+
+  def report_error(e)
+    Rails.logger.error "\n\n\n#{e}\n\n"
+    if defined? Airbrake
+      Airbrake.notify e
+    end
+    raise e
+  end
 
   # the aliasing prevents i.e. proposals_controller to change the condition
   alias fund_action_authenticate_user! authenticate_user!
