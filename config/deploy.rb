11e986ac0ceb469926ceffe0c682cc30526f1c6f
Jens Kraemer <jk@jkraemer.net>
2018-07-14 10:48:26 +0200

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      sudo "/etc/init.d/unicorn reload fundaction"
-      sudo "/etc/init.d/delayed_job restart fundaction"
+      sudo "/bin/systemctl reload fundaction_unicorn"
+      sudo "/bin/systemctl reload fundaction_delayed_job"
     end
   end
 
