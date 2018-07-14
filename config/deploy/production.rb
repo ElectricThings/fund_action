5993ecd21cdf413dbbbebe2969fe4a8cb015d129
Jens Kraemer <jk@jkraemer.net>
2018-07-14 07:57:15 +0200

    local config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-server 'webapps.wintermute', user: 'deploy', roles: %w{web app db}
+server 'case.jkraemer.net', user: 'deploy', roles: %w{web app db}
 
-set :ssh_options, {
-  keys: %w(/home/jk/.ssh/id_ed25519),
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
-
+# set :ssh_options, {
+#   keys: %w(/home/jk/.ssh/id_ed25519),
+#   forward_agent: true,
+#   auth_methods: %w(publickey)
+# }
+# 
