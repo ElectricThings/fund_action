ff9b5af8c5624cfb42f7f4986515cf4681f3e6bf
Jens Kraemer <jk@jkraemer.net>
2018-10-25 10:39:08 +0800

    update airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -4,20 +4,24 @@ if defined?(Airbrake)
 
   Airbrake.configure do |config|
     config.project_id = 1234
-    config.project_key = CGI.escape({
+    config.project_key = 'secret'
+    config.host = 'https://rm.jkraemer.net'
+    config.root_directory = Rails.root.to_s
+    config.environment = Rails.env
+    config.ignore_environments = %w(test) # development)
+
+    config.blacklist_keys |= Rails.application.config.filter_parameters.map(&:to_s)
+  end
+
+  Airbrake.add_filter do |notice|
+    notice[:context][:redmine_config] = {
       project: 'fund-action',
       tracker: 'Application Error',
       api_key: Rails.application.secrets.redmine_api_key,
       assigned_to: 'jk',
       priority: 3,
       environment: 'production'
-    }.to_json)
-    config.host = 'https://rm.jkraemer.net'
-    config.root_directory = Rails.root.to_s
-    config.environment = Rails.env
-    config.ignore_environments = %w(test development)
-
-    config.blacklist_keys |= Rails.application.config.filter_parameters.map(&:to_s)
+    }
   end
 
   Airbrake.add_filter AirbrakeFilter.new
