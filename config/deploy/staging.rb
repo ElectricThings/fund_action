6abaf26c18cbc0e2767f91e82db116621a2a3db0
Jens Kraemer <jk@jkraemer.net>
2019-05-22 08:46:14 +0800

    deploy calendar branch to stage

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 set :deploy_to, "/srv/webapps/#{fetch :application}_stage"
 set :script_postfix, "_stage"
 set :rails_env, :production
+set :branch, 'alabs-feature/decidim-calendar'
 
 server 'case.jkraemer.net', user: 'deploy', roles: %w{web app db}
 
