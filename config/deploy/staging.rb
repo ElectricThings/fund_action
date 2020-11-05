50b9ce4e3c87fe1c72c475c45dbcafe14349f509
Jens Kraemer <jk@jkraemer.net>
2020-11-05 11:13:29 +0800

    remove unused deployment target

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,5 @@ set :script_postfix, "_stage"
 set :rails_env, :production
 set :branch, 'staging'
 
-#server 'app01.jkraemer.net', user: 'deploy', roles: %w{web app}
 server 'wintermute.jkraemer.net', user: 'deploy', roles: %w{web app db}
 
