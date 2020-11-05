627a320df50da747bb23f3c08b861e3d5d6a29e3
Jens Kraemer <jk@jkraemer.net>
2020-11-05 11:00:06 +0800

    bump 0.22

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,8 @@
 set :deploy_to, "/srv/webapps/#{fetch :application}"
 set :branch, 'master'
 
-server 'app01.jkraemer.net', user: 'deploy', roles: %w{web app}
-server 'app03.jkraemer.net', user: 'deploy', roles: %w{web app db}
-#server 'case.jkraemer.net', user: 'deploy', roles: %w{web app db}
+server 'app02.jkraemer.net', user: 'deploy', roles: %w{web app}
+server 'wintermute.jkraemer.net', user: 'deploy', roles: %w{web app db}
 
 # set :ssh_options, {
 #   keys: %w(/home/jk/.ssh/id_ed25519),
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 set :deploy_to, "/srv/webapps/#{fetch :application}_stage"
 set :script_postfix, "_stage"
 set :rails_env, :production
-set :branch, 'master'
+set :branch, 'staging'
 
 #server 'app01.jkraemer.net', user: 'deploy', roles: %w{web app}
-server 'app03.jkraemer.net', user: 'deploy', roles: %w{web app db}
+server 'wintermute.jkraemer.net', user: 'deploy', roles: %w{web app db}
 
